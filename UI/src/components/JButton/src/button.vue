d5448376914b4d076acd5952f8947cb8bf7628d8
zhengjie <3426571530@qq.com>
2025-04-14 01:13:56 +0800

    feat: button样式打通

--- UI/src/components/JButton/src/button.vue
+++ UI/src/components/JButton/src/button.vue
@@ -32,8 +32,6 @@ const buttonKls = computed(() => [
   ns.is("round", props.round),
 ]);
 
-console.log(_type, _size, _ref, _tag, _props, _disabled);
-
 defineExpose({
   /** @description button html element */
   ref: _ref,
@@ -47,50 +45,5 @@ defineExpose({
 </script>
 
 <style lang="scss" scoped>
-button {
-  &.size-small {
-    font-size: 12px;
-    padding: 4px 8px;
-  }
-
-  &.size-medium {
-    font-size: 14px;
-    padding: 6px 12px;
-  }
-
-  &.size-large {
-    font-size: 16px;
-    padding: 8px 16px;
-  }
-
-  &.type-primary {
-    background-color: #409eff;
-    color: #fff;
-  }
-
-  &.type-success {
-    background-color: #67c23a;
-    color: #fff;
-  }
-
-  &.type-warning {
-    background-color: #e6a23c;
-    color: #fff;
-  }
-
-  &.type-danger {
-    background-color: #f56c6c;
-    color: #fff;
-  }
-
-  &.is-disabled {
-    cursor: not-allowed;
-    background-color: #dcdfe6;
-    color: #c0c4cc;
-  }
-
-  &.is-round {
-    border-radius: 20px;
-  }
-}
+@import "../../JStyle/src/button.scss";
 </style>
